4ec004087949da7cc347db063cd010819218a44b
hariprasatham <hariprasatham@gmail.com>
2025-10-17 06:37:31 +0530

    import updated for image compression

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { imagetools } from 'vite-imagetools'
 
 // https://vite.dev/config/
 export default defineConfig({
